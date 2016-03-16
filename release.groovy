572ba1acbe7ec9d8518f83821ccda457af031de3
rawlingsj <rawlingsj80@gmail.com>
2016-03-16 12:10:53 +0000

    add deploy stage

--- release.groovy
+++ release.groovy
@@ -1,5 +1,6 @@
 #!/usr/bin/groovy
 def updateDependencies(source){
+
   def properties = []
   properties << ['<pipeline.test.project.version>','io/fabric8/pipeline-test-project']
   properties << ['<docker.maven.plugin.version>','io/fabric8/docker-maven-plugin']
@@ -15,6 +16,24 @@ def stage(thisProject){
   return stageProject{
     project = 'fabric8io/pipeline-test-downstream-project'
     useGitTagForNextVersion = true
+  }
+}
+
+def deploy(project){
+  deployProject{
+    stagedProject = project
+    resourceLocation = 'target/classes/kubernetes.json'
+    environment = 'fabric8-preprod'
+  }
+}
+
+def approve(project){
+  def releaseVersion = project[1]
+  approve{
+    room = null
+    version = releaseVersion
+    console = fabric8Console
+    environment = 'fabric8-preprod'
   }
 }
 
